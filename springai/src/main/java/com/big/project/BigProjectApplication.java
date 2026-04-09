c5fcc428c3c6dbc43b414b8f4753df150fa8a2a5
zhaojunyi <zhaojunyi@pengzhirui.com>
2026-04-09 14:12:31 +0800

    first commit

---
+++ springai/src/main/java/com/big/project/BigProjectApplication.java
@@ -0,0 +1,13 @@
+package com.big.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigProjectApplication.class, args);
+    }
+
+}
---
+++ springai/src/test/java/com/big/project/BigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.big.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
