6b28ed390333420cfa20a4f97493f721ede8839d
zhaojunyi0016 <451108902@qq.com>
2026-05-25 09:33:49 +0800

    init

--- src/main/java/com/learn/harness/core/AgentLoopEngine.java
+++ src/main/java/com/learn/harness/core/AgentLoopEngine.java
@@ -182,7 +182,7 @@ public class AgentLoopEngine {
     }
 
     /**
-     * 调用模型
+     * 调用 模型
      */
     private AssistantMessage callModel(List<Message> messages) {
         ChatResponse response = chatClient.prompt()
